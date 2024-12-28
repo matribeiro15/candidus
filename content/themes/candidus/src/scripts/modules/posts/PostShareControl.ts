e51440f90b5fed5b02edb6568f1f19983b29d26d
Tq-bit <t.quante@outlook.com>
2024-12-28 13:55:27 +0100

    docs: add documentation to the rest of posts

--- content/themes/candidus/src/scripts/modules/posts/PostShareControl.ts
+++ content/themes/candidus/src/scripts/modules/posts/PostShareControl.ts
@@ -5,26 +5,42 @@ interface PostShareControlSelectors {
   toolbarContainer: string;
 }
 
+/**
+ * @class PostShareControl
+ * @extends Control
+ * @implements ControlStruct
+ * @description Manages the visibility of the toolbar container based on device type and scroll direction.
+ */
 export default class PostShareControl extends Control implements ControlStruct {
   toolbarContainer: HTMLElement;
   device: UserDevice;
 
+  /**
+   * @constructor
+   * @param {PostShareControlSelectors} param0 - The selectors for toolbar container.
+   */
   constructor({ toolbarContainer }: PostShareControlSelectors) {
     super(toolbarContainer);
     this.toolbarContainer = this.$query(toolbarContainer);
     this.device = new UserDevice()
   }
 
+  /**
+   * @desc Initialize the control and subscribe to device changes if on a smaller device.
+   */
   init(): void {
-    // TODO: Possible optimisation - remove subscribers when device type changes
     const isSmallerDevice = this.device.getDeviceType() === "mobile-sm"
-      || this.device.getDeviceType() === "mobile-sm"
+      || this.device.getDeviceType() === "mobile-md"
       || this.device.getDeviceType() === "tablet-sm";
     if (this.valid && isSmallerDevice) {
       this.device.addSubscriber(this.toggleToolbarContainer)
     }
   }
 
+  /**
+   * @desc Toggle the visibility of the toolbar container based on the scroll direction.
+   * @private
+   */
   private toggleToolbarContainer = () => {
     if (this.device.lastScrollDirection === "down") {
       this.toolbarContainer.classList.add('toolbar--responsive--hidden');
--- content/themes/candidus/src/scripts/modules/posts/PostSocialControl.ts
+++ content/themes/candidus/src/scripts/modules/posts/PostSocialControl.ts
@@ -9,23 +9,61 @@ type SocialPostElement =
 	| 'shareButtonReddit'
 	| 'shareButtonAny';
 
+/**
+ * A class representing a social media sharing control on a post.
+ * @class
+ */
 export default class PostSocialControl extends Control implements ControlStruct {
+	/**
+	 * The window options for the share pop-up window.
+	 */
 	private windowOptions = 'menubar=no,status=no,height=750,width=500';
+
+	/**
+	 * The post title element.
+	 */
 	private postTitle: HTMLElement;
+
+	/**
+	 * The post abstract element.
+	 */
 	private postAbstract: HTMLElement | null;
+
+	/**
+	 * The share button for Facebook.
+	 */
 	private shareButtonFacebook: HTMLElement;
+
+	/**
+	 * The share button for Twitter.
+	 */
 	private shareButtonTwitter: HTMLElement;
-	private shareButtonLinkedin: HTMLElement;
+
+	/**
+	 * The share button for Reddit.
+	 */
 	private shareButtonReddit: HTMLElement;
+
+	/**
+	 * The share button for LinkedIn.
+	 */
+	private shareButtonLinkedin: HTMLElement;
+
+	/**
+	 * The share button for any other platform.
+	 */
 	private shareButtonAny: HTMLElement;
 
+	/**
+	 * The constructor for the social media sharing control on a post.
+	 */
 	constructor({
 		postTitle,
 		postAbstract,
 		shareButtonFacebook,
 		shareButtonTwitter,
-		shareButtonReddit,
 		shareButtonLinkedin,
+		shareButtonReddit,
 		shareButtonAny,
 	}: Record<SocialPostElement, string>) {
 		super(
@@ -45,6 +83,9 @@ export default class PostSocialControl extends Control implements ControlStruct 
 		this.shareButtonAny = this.$query(shareButtonAny);
 	}
 
+	/**
+	 * Initializes the control by adding event listeners to the share buttons.
+	 */
 	async init() {
 		if (this.valid) {
 			this.initFacebookButton();
@@ -55,22 +96,37 @@ export default class PostSocialControl extends Control implements ControlStruct 
 		}
 	}
 
+	/**
+	 * Initializes the Facebook share button.
+	 */
 	private initFacebookButton() {
 		this.shareButtonFacebook.addEventListener('click', () => this.shareToFacebook());
 	}
 
+	/**
+	 * Initializes the Twitter share button.
+	 */
 	private initTwitterButton() {
 		this.shareButtonTwitter.addEventListener('click', () => this.shareToTwitter());
 	}
 
+	/**
+	 * Initializes the Reddit share button.
+	 */
 	private initRedditButton() {
 		this.shareButtonReddit.addEventListener('click', () => this.shareToReddit());
 	}
 
+	/**
+	 * Initializes the LinkedIn share button.
+	 */
 	private initLinkedinButton() {
 		this.shareButtonLinkedin.addEventListener('click', () => this.shareToLinkedin());
 	}
 
+	/**
+	 * Initializes or hides the any other platform share button.
+	 */
 	private initOrHideShareButtonAny() {
 		if (navigator.share) {
 			this.shareButtonAny.addEventListener('click', () => this.shareToAny());
@@ -79,6 +135,9 @@ export default class PostSocialControl extends Control implements ControlStruct 
 		}
 	}
 
+	/**
+	 * Shares the post to Facebook.
+	 */
 	private shareToFacebook() {
 		window.open(
 			`https://www.facebook.com/sharer.php?u=${window.location.href}`,
@@ -87,6 +146,9 @@ export default class PostSocialControl extends Control implements ControlStruct 
 		);
 	}
 
+	/**
+	 * Shares the post to Twitter.
+	 */
 	private shareToTwitter() {
 		window.open(
 			`
@@ -96,6 +158,9 @@ export default class PostSocialControl extends Control implements ControlStruct 
 		);
 	}
 
+	/**
+	 * Shares the post to Reddit.
+	 */
 	private shareToReddit() {
 		window.open(
 			`https://www.reddit.com/submit?url=${window.location.href}&title=${this.postTitle?.innerHTML}`,
@@ -104,6 +169,9 @@ export default class PostSocialControl extends Control implements ControlStruct 
 		);
 	}
 
+	/**
+	 * Shares the post to LinkedIn.
+	 */
 	private shareToLinkedin() {
 		window.open(
 			`https://www.linkedin.com/sharing/share-offsite/?url=${window.location.href}`,
@@ -112,6 +180,9 @@ export default class PostSocialControl extends Control implements ControlStruct 
 		);
 	}
 
+	/**
+	 * Shares the post to any other platform.
+	 */
 	private shareToAny() {
 		navigator.share({
 			title: this.postTitle?.innerText,
@@ -120,3 +191,4 @@ export default class PostSocialControl extends Control implements ControlStruct 
 		});
 	}
 }
+
